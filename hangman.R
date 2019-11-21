07707c940d835244acd5b580fe76d5a805a545bc
calejero <58038280+calejero@users.noreply.github.com>
2019-11-21 15:22:45 +0100

    Code style

--- hangman.R
+++ hangman.R
@@ -24,7 +24,7 @@ zamenjaj2 <- function(beseda, crka, iskana_beseda){
     
     pozicija <- regexpr(crka, beseda)[1]
     iskana_beseda[pozicija] <- crka
-    print(paste(iskana_beseda, collapse = " "))
+    print(paste(iskana_beseda, collapse =  " "))
     
     #convert back to single string to check for equality
     if (paste(iskana_beseda, collapse = "") == beseda) {
@@ -33,26 +33,25 @@ zamenjaj2 <- function(beseda, crka, iskana_beseda){
     }
     
     return(iskana_beseda)
-    
   }
 }
 
-drawHead <- function(orig_position = c(0,0)
-                     ,dia = 1
-                     ,nof_points = 10
-                     , group=5){
+drawHead <- function(orig_position = c(0,0),
+                     dia = 1,
+                     nof_points = 10,
+                     group = 5){
   
   vectT <- seq(0,2*pi, length.out = nof_points)
-  r = dia/2
+  r <- dia/2
   x_data <- orig_position[1] + r * cos(vectT)
   y_data <- orig_position[2] + r * sin(vectT)
-  return(data.frame (x = x_data, y = y_data, group=group))
+  return(data.frame (x = x_data, y = y_data, group = group))
 }
 
 drawMan <- function(st_napak) {
   
-  ggplot(levels[which(levels$group<=st_napak),], aes(x=x, y=y, group=group)) + 
-    geom_path(size=2.5) + 
+  ggplot(levels[which(levels$group <= st_napak), ], aes(x = x, y = y, group = group)) + 
+    geom_path(size = 2.5) + 
     theme_void()
   
 }
@@ -60,21 +59,26 @@ drawMan <- function(st_napak) {
 ########################
 ###### Data for graph
 ########################
-level1 <- data.frame(x=c(1,2,3,4,5,6,7,8), y=c(1,1,1,1,1,1,1,1), group=c(1,1,1,1,1,1,1,1))
-level2 <- data.frame(x=c(4,4,4,4,4), y=c(1,2,3,4,5),group=c(2,2,2,2,2))
-level3 <- data.frame(x=c(4,5,6), y=c(5,5,5), group=c(3,3,3))
-level4 <- data.frame(x=c(6,6), y=c(5,4), group=c(4,4))
-level5 <- drawHead(c(6,3.5),1,10,5)
-level6 <- data.frame(x=c(6,6,5.8,6.2),y=c(3,1.5,1.5,1.5), group=c(6,6,6,6))
-level7 <- data.frame(x=c(5.5,6,6.5),y=c(2,2.5,2), group=c(7,7,7))
-levels <- rbind(level1,level2,level3,level4,level5,level6,level7)
-rm(level1,level2,level3,level4,level5,level6,level7)
+level1 <- data.frame(x = c(1, 2, 3, 4, 5, 6, 7, 8), 
+                     y = c(1, 1, 1, 1, 1, 1, 1, 1), 
+                     group = c(1, 1, 1, 1, 1, 1, 1, 1))
+level2 <- data.frame(x = c(4, 4, 4, 4, 4), 
+                     y = c(1, 2, 3, 4, 5),
+                     group = c(2, 2, 2, 2, 2))
+level3 <- data.frame(x = c(4, 5, 6), y= c (5, 5, 5), group = c(3, 3, 3))
+level4 <- data.frame(x = c(6, 6), y = c(5, 4), group = c(4, 4))
+level5 <- drawHead(c(6, 3.5), 1, 10, 5)
+level6 <- data.frame(x = c(6, 6, 5.8, 6.2), 
+                     y =c 3,1.5,1.5,1.5), group = c(6, 6, 6, 6))
+level7 <- data.frame(x = c(5.5, 6, 6.5), y = c(2, 2.5, 2), group = c(7, 7, 7))
+levels <- rbind(level1, level2, level3, level4, level5, level6, level7)
+rm(level1, level2, level3, level4, level5, level6, level7)
 
 ########################
 ### Helper variables
 ########################
 
-rm(st_napak, izbor, crka,cilj,cilj_n, iskana_beseda,beseda,i,active)
+rm(st_napak, izbor, crka, cilj, cilj_n, iskana_beseda, beseda, i, active)
 st_napak = 0
 i = 0
 izbor = NULL
@@ -87,7 +91,7 @@ active = TRUE
 #######################
 
 StartNewGame <- function(sensitive.flag = TRUE) { # sensitive.flag: TRUE -> capital letters are available. 
-  beseda <- readline(prompt="Word: ")
+  beseda <- readline(prompt = "Word: ")
   
   if (sensitive.flag == FALSE) {
     beseda <- base::tolower(beseda)
@@ -97,7 +101,7 @@ StartNewGame <- function(sensitive.flag = TRUE) { # sensitive.flag: TRUE -> capi
   
   #iskana_beseda <- replicate(nchar(beseda),'_')
   #iskana_beseda <- toString(as.character(replicate(nchar(beseda), ' _ ')))
-  iskana_beseda <- replicate(nchar(beseda),'_')
+  iskana_beseda <- replicate(nchar(beseda), '_')
   
   
   while (active == TRUE) {
@@ -125,25 +129,25 @@ StartNewGame <- function(sensitive.flag = TRUE) { # sensitive.flag: TRUE -> capi
       
     } else {
       cilj_n <- rbind(cilj_n, crka)
-      print(paste("Nope!","Try N:",i+1,"Wrong letters: {",(toString(paste0(cilj_n, sep=","))),"}")) 
+      print(paste("Nope!","Try N:",i + 1, "Wrong letters: {",(toString(paste0(cilj_n, sep = ","))),"}")) 
       #print(toString(paste0(cilj_n, sep=",")))
       
       #Graph
       st_napak <- as.integer(length(cilj_n))
       print(drawMan(st_napak = st_napak))
       
-      if(as.integer(st_napak)==7){
-        active==FALSE
+      if(as.integer(st_napak) == 7){
+        active == FALSE
         break
         print("End Game")
       }
       
     }
     
-    i= i+1
+    i= i + 1
     #cat("\f")  
-    if(st_napak==7){
-      active==FALSE
+    if(st_napak == 7){
+      active == FALSE
       break
       print("End game")
     }
